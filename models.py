5fe08b5922b31e336aa2b0794719883dcf460341
Timothy Cottrell <timwcottrell8370405@gmail.com>
2021-11-08 22:00:21 -0500

    Convertion!

--- models.py
+++ models.py
@@ -2,45 +2,62 @@ from database import db
 from sqlalchemy.dialects.sqlite import BLOB
 
 class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column("id", db.Integer, primary_key=True)
-    email = db.Column("email", db.String(100))
-    username = db.Column("username", db.String(100))
-    fname = db.Column("fname", db.String(20))
-    lname = db.Column("lname", db.String(20))
-    password = db.Column("password", db.String(30))
-    recruiter = db.Column("recruiter", db.Boolean)
-    resumes = db.relationship("Resume", back_populates="user")
+   __tablename__ = 'User'
+   id = db.Column("id", db.Integer, primary_key=True)
+   email = db.Column("email", db.String(100))
+   username = db.Column("username", db.String(100))
+   fname = db.Column("fname", db.String(20))
+   lname = db.Column("lname", db.String(20))
+   password = db.Column("password", db.String(30))
+   recruiter = db.Column("recruiter", db.Boolean)
+   resume = db.relationship("Resume", backref="User")
 
-    def __init__(self, fname, lname, username, email, pwd, is_recruiter):
-        self.email = email
-        self.username = username
-        self.fname = fname
-        self.lname = lname
-        self.password = pwd
-        self.recruiter = is_recruiter
+   def __init__(self, fname, lname, username, email, pwd, is_recruiter):
+       self.email = email
+       self.username = username
+       self.fname = fname
+       self.lname = lname
+       self.password = pwd
+       self.recruiter = is_recruiter
 
-    def __repr__(self):
-        return f"User('{self.id}', '{self.email}', '{self.username}', '{self.fname}', '{self.lname}','{self.password}')"
+   def __repr__(self):
+       return f"User('{self.id}', '{self.email}', '{self.username}', '{self.fname}', '{self.lname}','{self.password}')"
 
 
 #Just a start still gotta lot of planning to do
 class Resume(db.Model):
-    __tablename__ = 'resume'
-    id = db.Column("id", db.Integer, primary_key=True)
-    user_id = db.Column("user_id", db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship("User", back_populates="resumes")
-    html = db.Column('html', BLOB)
-    category = db.Column("category", db.String(50))
-    text = db.Column('text', db.String(300))
-    headers = db.Column("headers", db.String(300))
+   __tablename__ = 'Resume'
+   id = db.Column("id", db.Integer, primary_key=True)
+   user_id = db.Column("user_id", db.Integer, db.ForeignKey('User.id'))
+   html = db.Column('html', BLOB)
+   category = db.Column("category", db.String(50))
+   text = db.relationship("Text", backref="Resume")
 
-    def __init__(self, user_id, html_in, cat, text_in, headers_in):
-        self.user_id = user_id
-        self.html = html_in
-        self.category = cat
-        self.text = text_in
-        self.headers = headers_in
+   def __init__(self, user_id, html_in, cat):
+       self.user_id = user_id
+       self.html = html_in
+       self.category = cat
 
-    def __repr__(self):
-        return f"Resume('{self.id}', '{self.category}', '{self.text}', '{self.headers}')"
+   def __repr__(self):
+       return f"Resume('{self.id}', '{self.category}', '{self.texts}')"
+
+class Text(db.Model):
+   __tablename__ = 'Text'
+   id = db.Column("id", db.Integer, primary_key=True)
+   word = db.Column("word", db.String(50))
+   count = db.Column("count", db.Integer)
+   isHead = db.Column("ishead", db.Boolean)
+   head = db.Column("head", db.String(50))
+   resume_id = db.Column("resume_id", db.Integer, db.ForeignKey('Resume.id'))
+
+
+
+   def __init__(self, word, count, isHead, head, resume_id):
+       self.word = word
+       self.count = count
+       self.isHead = isHead
+       self.head = head
+       self.resume_id = resume_id
+
+   def __repr__(self):
+       return f"Text('{self.word}', '{self.count}', '{self.isHead}', '{self.head}')"
